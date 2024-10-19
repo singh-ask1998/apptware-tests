a5c38737d125f0e5a439a00b2e86e6ad23964fe8
singh-ask1998 <singh.ask1998@gmail.com>
2024-10-20 01:16:35 +0530

    Fixed DataReaderImpl.java

--- src/main/java/com/apptware/interview/stream/impl/DataReaderImpl.java
+++ src/main/java/com/apptware/interview/stream/impl/DataReaderImpl.java
@@ -16,26 +16,25 @@ class DataReaderImpl implements DataReader {
 
   @Override
   public Stream<String> fetchLimitadData(int limit) {
-    return fetchPaginatedDataAsStream().limit(limit);
+    //passing limit so that only limited data is fetched from pagination service making this operation more efficient.
+    return fetchPaginatedDataAsStream(limit).limit(limit);
   }
 
   @Override
   public Stream<String> fetchFullData() {
-    return fetchPaginatedDataAsStream();
+    //passing null so method fetches all data 
+    return fetchPaginatedDataAsStream(null);
   }
 
   /**
    * This method is where the candidate should add the implementation. Logs have been added to track
    * the data fetching behavior. Do not modify any other areas of the code.
    */
-  private @Nonnull Stream<String> fetchPaginatedDataAsStream() {
+  //adding one more attribute so that solution is more efficient in case of needing limited data.
+  private @Nonnull Stream<String> fetchPaginatedDataAsStream(Integer limit) {
     log.info("Fetching paginated data as stream.");
-
-    // Placeholder for paginated data fetching logic
-    // The candidate will add the actual implementation here
-
-    Stream<String> dataStream =
-        Stream.empty(); // Temporary, will be replaced by the actual data stream
+       
+    Stream<String> dataStream =  paginationService.getPaginatedData(1, limit!=null?limit:PaginationService.FULL_DATA_SIZE).stream();
     return dataStream.peek(item -> log.info("Fetched Item: {}", item));
   }
 }
